d2771f1351218264f0de946cf19f629719e4fcee
ABDULAZIZ20212 <94677302+ABDULAZIZ20212@users.noreply.github.com>
2022-11-06 21:58:31 +0300

    Add files via upload

---
+++ Streamlit_app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+
+#  Preparing streamlit app page.
+st.title('Hello world')
+st.subheader('Step 1')
+if st.button('Click'):
+   st.write('Some content')
+st.subheader('Step 2')
+
+#  Preparing Uploader for datasets or files.
+files = st.file_uploader('Upload data',type=['csv', 'xlsx', 'png', 'jpeg', 'gif', 'docx','pdf'],accept_multiple_files=True)
+for file in files:
+ if file.type=='text/csv':
+     df = pd.read_csv(file)
+     file.seek(0)
+     st.write(df)
+     file_details = {"FileName":file.name,"FileType":file.type}
+     st.write(file_details)
+
+#  Make a connection between client and GCP (Google Cloud Platform) to hosting datasets or files.
+if files:
+     credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"]
+        )
+     client = storage.Client(credentials=credentials)
+     bucket = client.get_bucket("dataset5320")
+     blob = bucket.blob(file.name)
+     blob.upload_from_string(file.getvalue(), content_type=file.type)
+     st.success("Nice!! Your Dataset has been uploaded Successfully")
