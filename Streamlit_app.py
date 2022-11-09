3c34999470ca6ad5593b4aa687d89c5ad2068101
ABDULAZIZ20212 <94677302+ABDULAZIZ20212@users.noreply.github.com>
2022-11-09 08:43:10 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
 
-import streamlit as st
+credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"]
+        )
 st.write('**Secret:**',st.secrets['secret'])
 st.write('**Password:**',st.secrets['section']['password']) 
