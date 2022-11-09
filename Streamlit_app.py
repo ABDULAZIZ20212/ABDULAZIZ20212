4d5f4ab5e92f187d20015308fdd5ef0e2e4e14c5
ABDULAZIZ20212 <94677302+ABDULAZIZ20212@users.noreply.github.com>
2022-11-09 08:41:00 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,57 +3,6 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
 
-#  Preparing streamlit app page.
-st.title('Hello world')
-st.subheader('Step 1')
-
-#  Preparing Uploader for datasets or files.
-files = st.file_uploader('Upload Your Data',type=['csv', 'xlsx', 'png', 'jpeg', 'jpg' ,'gif', 'docx','pdf'],accept_multiple_files=True)
-for file in files:
- if file.type=='text/csv':
-     df = pd.read_csv(file)
-     file.seek(0)
-     st.write(df)
-     file_details = {"FileName":file.name,"FileType":file.type}
-     st.write(file_details)
- elif file.type=='image/png':
-     file.seek(0)
-     st.image(file)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
- elif file.type=='image/jpg':
-     file.seek(0)
-     st.image(file)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
- elif file.type=='image/jpeg':
-     file.seek(0)
-     st.image(file)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
- elif file.type=='image/gif':
-     file.seek(0)
-     st.image(file)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
- elif file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-     df = pd.read_excel(file)
-     file.seek(0)
-     st.write(df)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
- else:
-     file.seek(0)
-     st.write(file)
-     file_details = {"FileName": file.name, "FileType": file.type}
-     st.write(file_details)
-    #  Make a connection between client and GCP (Google Cloud Platform) to hosting datasets or files.
- if files:
-     credentials = service_account.Credentials.from_service_account_info(
-            st.secrets["gcp_service_account"]
-        )
-     client = storage.Client(credentials=credentials)
-     bucket = client.get_bucket("dataset5320")
-     blob = bucket.blob(file.name)
-     blob.upload_from_string(file.getvalue(), content_type=file.type)
-     st.success("Nice!! Your Dataset has been uploaded successfully to cloud")
+import streamlit as st
+st.write('**Secret:**',st.secrets['secret'])
+st.write('**Password:**',st.secrets['section']['password']) 
