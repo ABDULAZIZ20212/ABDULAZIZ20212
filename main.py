7f2e27cd44f021bcffabb6d0f79d80861a674a6a
ABDULAZIZ20212 <94677302+ABDULAZIZ20212@users.noreply.github.com>
2022-11-22 19:15:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 # streamlit_app.py
-import datetime,time
-import os
+
 import streamlit as st
 from google.oauth2 import service_account
-from google.cloud import storage
+from google.cloud import bigquery
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
-client = storage.Client(credentials=credentials)
+client = bigquery.Client(credentials=credentials)
 
-# Retrieve file contents.
+# Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-# def read_file(bucket_name, file_path):
-bucket = client.get_bucket("dataset5320")
-#     content = bucket.blob(file_path).download_as_string().decode("utf-8")
-#     return content
-#
-# bucket_name = "dataset5320"
-file_path = ("exams10.csv")
-# blob = bucket.blob(file_path)
-# with open('C:\\Users\\MY PC\\Desktop\\exams.csv')as f:
-#     blob.upload_from_file(f)
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.experimental_memo to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-blob = bucket.blob(file_path)
-blob.upload_from_filename('Vs.csv',content_type='text/csv')
-st.write("upload complete")
+rows = run_query("CREATE OR REPLACE EXTERNAL TABLE 'myproject.mydataset.mytable'
+     OPTIONS( format='CSV', uris=['gs://mybucket/*.csv']))
 
-# print("upload complete")
-# content = read_file(bucket_name, file_path)
-# df.to_csv()
-# bucket.blob ( 'tsla( 0 ).csv'.format(datetime.datetime.now( ).strftime('%Y-%m-%d %H_%M_%s'))).upload_from_string('text/csv')
+
 # Print results.
-# st.write(content)
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['gender'])
+
+    # intricate - reef - 367307.1.exams
